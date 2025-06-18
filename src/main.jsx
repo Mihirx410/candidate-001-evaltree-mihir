a0a614b00b0ca182db33b5407dfaac6fce5d7326
Mihir Dodiya <mihirdodiya0077@gmail.com>
2025-06-18 16:58:03 +0530

    changes in main.jsx for worker file issue live

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser');
-  await worker.start();
-}
+import { worker } from './mocks/browser';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+worker.start().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
